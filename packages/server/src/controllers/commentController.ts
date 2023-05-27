491ac7cba9b894a0ea63296d6ed5bd2bdd252077
Letam Bossman Barinua <letambarinua@gmail.com>
2023-05-27 10:34:15 +0100

    Create editComment controller for editing comments

--- packages/server/src/controllers/commentController.ts
+++ packages/server/src/controllers/commentController.ts
@@ -7,7 +7,7 @@ import { commentFormValidation } from "../middleware/formValidation";
 
 // @access Private
 // @desc Create comment on blog post
-// @route POST /api/v1/user/blog/:blogPostId/comment
+// @route POST /api/v1/user/comment/:blogPostId
 export const postComment = [
   protectRoute,
   ...commentFormValidation,
@@ -28,6 +28,36 @@ export const postComment = [
         const commentObj = await CommentModel.create(data);
         const commentId = commentObj._id;
         blogPost.comments.push(commentId);
+        res.status(201).json(commentObj);
+      } else {
+        res.status(401);
+        throw new Error("Oops! something unexpected happend");
+      }
+    } else res.status(401).json({ message });
+  }),
+];
+
+// @access Private
+// @desc Edit comment on blog post
+// @route PATCH /api/v1/user/comment/:commentId
+export const editComment = [
+  protectRoute,
+  ...commentFormValidation,
+  asyncHandler(async (req, res) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    const message = errors.array().map((err) => err.msg);
+
+    // Checks to see no error from client
+    if (errors.isEmpty()) {
+      const { comment } = req.body;
+      const { commentId } = req.params;
+      const commentAuthor = req.body.user._id;
+      const commentPost = await CommentModel.findById(commentId);
+
+      if (commentAuthor && commentPost) {
+        commentPost.comment = comment || commentPost.comment;
+        const commentObj = await commentPost.save();
         res.status(201).json(commentObj);
       } else {
         res.status(401);
